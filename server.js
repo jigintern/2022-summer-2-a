ecf24ac28e2c5517998d4c615530d7c5477df690
Taiki-Watanabe <taiki.w.1113@gmail.com>
2022-08-22 13:07:07 +0900

    年を追加

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-import { serve } from "https://deno.land/std@0.151.0/http/server.ts";
 import { serveDir } from "https://deno.land/std@0.151.0/http/file_server.ts";
+import { serve } from "https://deno.land/std@0.151.0/http/server.ts";
 
 serve(async (req) => {
   const pathname = new URL(req.url).pathname;
   console.log(pathname);
 
   if (req.method === "GET" && pathname === "/welcome-message") {
-    return new Response("jigインターンへようこそ！");
+    return new Response("2022jigインターンへようこそ！");
   }
 
   return serveDir(req, {
