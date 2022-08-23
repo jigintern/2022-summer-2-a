fe612187b8afde9043c7ab6b689a258b4be725ae
kiseki-kiseki <kcanggua@gmail.com>
2022-08-23 13:18:33 +0900

    ページ遷移実装

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,18 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import First from "@/pages/first"
+import First from "@/pages/first";
 import Wait from "./pages/wait";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<App/>}/>
-            <Route path="/wait" element={<Wait/>}/>
-            <Route path="/first" element={<First/>}/>
-        </Routes>
-    </BrowserRouter>
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<First />} />
+      <Route path="/wait" element={<Wait />} />
+    </Routes>
+  </BrowserRouter>
+);
--- frontend/src/pages/first.tsx
+++ frontend/src/pages/first.tsx
@@ -2,8 +2,13 @@ import React from "react";
 import { useState } from "react";
 import { A } from "@/pages/firstcss";
 import { css } from "@emotion/react";
+// import { useHistory } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
 const First = () => {
   const yahharo = "yahharo";
+  //   const history = useHistory();
+  const navigate = useNavigate();
   return (
     <>
       {yahharo}
@@ -11,6 +16,8 @@ const First = () => {
       <div css={A}>
         <h1>Hello</h1>
       </div>
+      <button onClick={() => navigate("/wait")}>Go to wait</button>
+      {/* <button onClick={() => history.push("/wait")}>Go to wait</button> */}
     </>
   );
 };
