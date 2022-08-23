4750994c6e9b6c3fcd01286300050c038f340152
kiseki-kiseki <kcanggua@gmail.com>
2022-08-23 12:48:00 +0900

    path alias設定

--- frontend/src/pages/first.tsx
+++ frontend/src/pages/first.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import { useState } from 'react';
-import { A } from "./firstcss"
-import { css } from '@emotion/react';
+import React from "react";
+import { useState } from "react";
+import { A } from "@/pages/firstcss";
+import { css } from "@emotion/react";
 const First = () => {
-    const yahharo = "yahharo"
-    return (
-        <>
-            {yahharo}
-            <h1>First</h1>
-            <div css={A}>
-                <h1>Hello</h1>
-            </div>
-
-        </>
-    )
-}
+  const yahharo = "yahharo";
+  return (
+    <>
+      {yahharo}
+      <h1>First</h1>
+      <div css={A}>
+        <h1>Hello</h1>
+      </div>
+    </>
+  );
+};
 
 export default First;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import tsconfigPaths from "vite-tsconfig-paths"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -10,5 +10,6 @@ export default defineConfig({
         plugins: ["@emotion/babel-plugin"],
       },
     }),
+    tsconfigPaths(),
   ],
 });
