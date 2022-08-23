3a483cb5e0c81a0dd2aeebf8385addce1f4d25e5
i19yanagi <i19yanagi@gmail.com>
2022-08-23 11:34:25 +0900

    WaitParticipantsのテスト

--- backend/entity/wait/participant/waitParticipants.test.ts
+++ backend/entity/wait/participant/waitParticipants.test.ts
@@ -12,14 +12,44 @@ class FakeSocket {
   };
 }
 
-Deno.test("追加のテスト", () => {
-  const socket: WebSocket = undefined as any;
-  const yoichi = new WaitParticipant("Yoichi", socket);
-  const ryu = new WaitParticipant("Ryu", socket);
+const yoichSocket= new FakeSocket();
+const yoichi = new WaitParticipant("Yoichi", yoichSocket as any);
+const ryuSocket = new FakeSocket();
+const ryu = new WaitParticipant("Ryu", ryuSocket as any);
 
+Deno.test("追加のテスト", () => {
   const participants = new WaitParticipants([yoichi]);
   assertEquals(
     JSON.stringify(participants.joined(ryu)),
     JSON.stringify(new WaitParticipants([yoichi, ryu])),
   );
 });
+
+Deno.test("clearのテスト", () => {
+  const participants = new WaitParticipants([yoichi]);
+  assertEquals(
+      JSON.stringify(participants.clear()),
+      JSON.stringify(new WaitParticipants())
+  )
+})
+
+Deno.test("脱退のテスト", () => {
+  const participants = new WaitParticipants([yoichi, ryu]);
+  assertEquals(
+      JSON.stringify(participants.disconnected(yoichSocket as any)),
+      JSON.stringify(new WaitParticipants([ryu]))
+  )
+})
+
+Deno.test("開始のテスト", () => {
+  const participants = new WaitParticipants([yoichi, ryu]);
+  participants.start();
+  assertEquals(
+      JSON.stringify(yoichSocket.sendHistories),
+      JSON.stringify(["start"])
+  )
+  assertEquals(
+      JSON.stringify(ryuSocket.sendHistories),
+      JSON.stringify(["start"])
+  )
+})
