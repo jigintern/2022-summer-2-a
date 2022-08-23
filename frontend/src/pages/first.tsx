58216cb053fdcc0d101850362208406d95e99efe
tanaka <lemo.nade.engineer.room@gmail.com>
2022-08-23 13:56:53 +0900

    Websocketに接続し、名前を送信できるようにした。

--- frontend/src/pages/first.tsx
+++ frontend/src/pages/first.tsx
@@ -1,21 +1,11 @@
 import React from "react";
-import { useState } from "react";
-import { A } from "@/pages/firstcss";
-import { css } from "@emotion/react";
-// import { useHistory } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 const First = () => {
-  const yahharo = "yahharo";
-  //   const history = useHistory();
   const navigate = useNavigate();
   return (
     <>
-      {yahharo}
       <h1>First</h1>
-      <div css={A}>
-        <h1>Hello</h1>
-      </div>
       <button onClick={() => navigate("/wait")}>Go to wait</button>
       {/* <button onClick={() => history.push("/wait")}>Go to wait</button> */}
     </>
--- frontend/src/pages/wait.tsx
+++ frontend/src/pages/wait.tsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { NameMessage } from "@/models/wait/nameMessage";
 
 const Wait = () => {
+  const navigate = useNavigate();
+  const socket = new WebSocket(`ws://${import.meta.env.VITE_HOST}/echo/wait`);
+  socket.onmessage = (e) => {
+    if (e.data === "start") {
+      navigate("/game");
+      return;
+    }
+    console.error("start以外が渡された: " + e.data);
+  };
+
+  const [name, setName] = useState("");
+
+  const sendName = (): void => {
+    new NameMessage(name).send(socket);
+  };
   return (
     <>
       <h1>Wait</h1>
+      <p>
+        <input
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+          placeholder="名前"
+        />
+        <button onClick={sendName}>決定</button>
+      </p>
     </>
   );
 };
 
-export default Wait
+export default Wait;
