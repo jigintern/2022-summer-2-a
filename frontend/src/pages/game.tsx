1f2dc7ed83e7105b6437f1e15e92307f1530c809
i19yanagi <i19yanagi@gmail.com>
2022-08-24 14:11:32 +0900

    wait画面での名前の入力値を保持

--- frontend/src/pages/game.tsx
+++ frontend/src/pages/game.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React　from "react";
 import CellsComponent from "@/conponents/game/cellsComponent";
 import {GameData} from "@/models/game/data/gameData";
 import {Cell} from "@/models/game/data/cell";
 import {Location} from "@/models/game/data/location";
+import {useLocation} from "react-router-dom";
 
 const Game = () => {
+    const location = useLocation();
+    const name = (location.state as {name: string}).name;
+    const wsProtocol = import.meta.env.VITE_PROTOCOL === "secure" ? "wss" : "ws";
+    const socket = new WebSocket(
+        `${wsProtocol}://${import.meta.env.VITE_HOST}/echo/wait`
+    );
+    socket.onopen = () => {
+        socket.send(JSON.stringify({type: "name", name: name}));
+    }
     const data = new GameData(
         undefined as any,
         undefined as any,
