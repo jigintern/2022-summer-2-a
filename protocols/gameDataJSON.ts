c81a8e0c18870f1cf00ae8b1cb0a16aa27b9a15f
kiseki-kiseki <kcanggua@gmail.com>
2022-08-24 13:04:43 +0900

    gameDataを型つけて作りました

--- protocols/gameDataJSON.ts
+++ protocols/gameDataJSON.ts
@@ -1,14 +1,18 @@
 export interface GameDataJSON {
   readonly participantCount: number;
   readonly cellCount: number;
-  readonly cells: {
-    readonly location: number;
-    readonly title: string;
-    readonly description: string;
-  }[];
+  readonly cells: CellData[];
   readonly participants: {
     readonly name: string;
     readonly location: number;
     readonly number: number;
   }[];
+}
+
+export interface CellData {
+  readonly location: number;
+  readonly title: string;
+  readonly description: string;
+  readonly rest: boolean;
+  readonly forward: number;
 }
