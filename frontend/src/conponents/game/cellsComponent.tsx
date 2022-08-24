dc816d4203253a17883ec7c1876e3416f1e295a6
yamamoto_k <19276@toba-cmt.ac.jp>
2022-08-24 11:21:08 +0900

    マス目を作成

---
+++ frontend/src/conponents/game/cellsComponent.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {GameData} from "@/models/game/data/gameData";
+import { css } from "@emotion/react"
+
+type Props = { data: GameData }
+
+const cellStyle = css`
+  width: 60px;
+  height: 60px;
+  background-color: white;
+  border: 1px solid black;
+`
+
+const CellsComponent = ({ data }: Props) => {
+    return (
+        <>
+            <h1>Cell!!!</h1>
+            {data.cells.map(cell=>{
+                return (<div css={cellStyle} key={cell.location.location}/>)
+            })}
+        </>
+    );
+};
+
+export default CellsComponent;
