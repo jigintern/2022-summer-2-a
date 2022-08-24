ab10e0ceb5854755187ff937b44bd9c58d8eaedd
i19yanagi <i19yanagi@gmail.com>
2022-08-24 11:43:03 +0900

    フォーマット

--- backend/routes/echo/game.ts
+++ backend/routes/echo/game.ts
@@ -3,42 +3,42 @@ import { Dice } from "~/entity/game/dice/dice.ts";
 import { GameParticipant } from "~/entity/game/participant/gameParticipant.ts";
 import { GameParticipants } from "~/entity/game/participant/gameParticipants.ts";
 interface User {
-    type: "roulette" | "join" | "data";
-    name: string;
+  type: "roulette" | "join" | "data";
+  name: string;
 }
 
 let participants: GameParticipants = new GameParticipants();
 
 const onMessageAction = (e: MessageEvent<string>, socket: WebSocket) => {
-    const data: User = JSON.parse(e.data);
-    if (data.type === "roulette") {
-        const dice = Dice.generate(data.name);
-        participants.notify(dice.notification());
-        return;
-    }
-    if (data.type === "join") {
-        const participant = new GameParticipant(data.name, socket);
-        participants = participants.joined(participant);
-        return;
-    }
-    console.error("Typeが間違っています");
+  const data: User = JSON.parse(e.data);
+  if (data.type === "roulette") {
+    const dice = Dice.generate(data.name);
+    participants.notify(dice.notification());
+    return;
+  }
+  if (data.type === "join") {
+    const participant = new GameParticipant(data.name, socket);
+    participants = participants.joined(participant);
+    return;
+  }
+  console.error("Typeが間違っています");
 };
 
 const onMessage = (e: MessageEvent<string>, socket: WebSocket) => {
-    onMessageAction(e, socket);
-    //データ送信操作
-}
+  onMessageAction(e, socket);
+  //データ送信操作
+};
 
 export const handler: Handlers = {
-    GET(req: Request, _ctx: HandlerContext) {
-        let response, socket: WebSocket;
-        try {
-            ({ response, socket } = Deno.upgradeWebSocket(req));
-        } catch {
-            return new Response("request isn't trying to upgrade to websocket.");
-        }
-        socket.onmessage = (e) => onMessage(e, socket);
-        socket.onerror = (e) => console.log("socket errored:", e);
-        return response;
-    },
+  GET(req: Request, _ctx: HandlerContext) {
+    let response, socket: WebSocket;
+    try {
+      ({ response, socket } = Deno.upgradeWebSocket(req));
+    } catch {
+      return new Response("request isn't trying to upgrade to websocket.");
+    }
+    socket.onmessage = (e) => onMessage(e, socket);
+    socket.onerror = (e) => console.log("socket errored:", e);
+    return response;
+  },
 };
